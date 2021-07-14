8913123962b39a2726066f010373e22c29e0554f
planeset94 <planeset94@gmail.com>
2021-07-14 16:55:28 +0200

    add Controller & autocompile its file

--- app/Comic.php
+++ app/Comic.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Comics extends Model
+class Comic extends Model
 {
-    protected $table = 'Comics';
+   protected $table='Comics';
 }
