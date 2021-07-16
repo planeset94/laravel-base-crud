3a62e81124a6bdb9077141ca26eb1f5ec194e58f
planeset94 <planeset94@gmail.com>
2021-07-16 09:24:09 +0200

    Bonus: pop up form - used bootstrap modal-

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -50,17 +50,50 @@
                             <a href="{{ route('comics.show', $comic->id) }}">View</a>
                             <a class="pl-1" href="{{ route('comics.edit', $comic->id) }}">Edit</a>
                             <br>
-                            <form class="pt-3" action="{{ route('comics.delete', $comic->id) }}" method="POST">
-                                @csrf
-                                @method('DELETE')
-                                <button type="submit" class="btn btn-outline-danger">Delete</button>
-                            </form>
+
+
+                            {{-- modal --}}
+                            <!-- Button trigger modal -->
+                            <button type="button" class="btn btn-danger" data-toggle="modal" data-target="#exampleModal">
+                                Delete
+                            </button>
+
+                            <!-- Modal -->
+                            <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel"
+                                aria-hidden="true">
+                                <div class="modal-dialog">
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                            <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                <span aria-hidden="true">&times;</span>
+                                            </button>
+                                        </div>
+                                        <div class="modal-body">
+                                            Permanently delete this record
+                                        </div>
+                                        <div class="modal-footer">
+                                            <button type="button" class="btn btn-primary" data-dismiss="modal">Go
+                                                Back</button>
+                                            <form action="{{ route('comics.delete', $comic->id) }}" method="POST">
+                                                @csrf
+                                                @method('DELETE')
+                                                <button type="submit" id="danger-button"
+                                                    class="btn btn-outline-danger">Delete</button>
+                                            </form>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            {{-- modal --}}
+
 
                         </td>
                     </tr>
                 @endforeach
             </tbody>
         </table>
+
 
 
 
